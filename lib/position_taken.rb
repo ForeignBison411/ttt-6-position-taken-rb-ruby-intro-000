85e705d336baf5fe07b33f24feb39fdb94d2a95a
Neil Brown <neilbrown176@gmail.com>
2020-05-11 05:17:52 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] != " " || ""
-    1 == 1
-  else
+  if board[index] == " " || ""
     1 != 1
+  else
+    1 == 1
   end
 end
